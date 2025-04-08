ba1e8858b2ec0191c1dfced2a01fb2d72ea913b7
dioginis <dioginiscarvalho@hotmail.com>
2025-04-07 22:10:40 -0300

    add: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,14 @@
+import { Container, GithubLogo, SearchForm } from './styles';
+
 const Header: React.FC = () => {
-    return <div />
+    return (
+        <Container>
+            <GithubLogo />
+            <SearchForm >
+                <input type="text" placeholder="Enter Username or Repo..." />
+            </SearchForm>
+        </Container>
+    );
 }
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
+import {FaGithub} from 'react-icons/fa';
 
 export const Container = styled.div`
-    max-width: 1280px;
+    display: flex;
+    align-items: center;
+    background: var(--header);
+    padding: 11px 16px;
 `;
+
+export const GithubLogo = styled(FaGithub)`
+    fill: var(--logo);
+    width: 32px;
+    height: 32px;
+    flex-shrink: 0;
+`;
+
+export const SearchForm = styled.form``;
